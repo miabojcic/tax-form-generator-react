e1064dd08cac466f955a7c3887c360bed4af125a
Mia <mia@laplacian.hr>
2019-06-18 12:37:53 +0200

    Login

---
+++ src/login/jwt-token.ts
@@ -0,0 +1,9 @@
+export interface JwtToken {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    exp: string;
+    iss: string;
+    aud: string;
+}
--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+import { Link, Redirect, Switch } from 'react-router-dom';
+import { Credentials } from './credentials';
+import axios from 'axios';
+import { LoginResponse } from './login-response';
+// @ts-ignore
+import * as jwt_decode from 'jwt-decode';
+import { User } from './user';
+import { JwtToken } from './jwt-token';
+import { useSnackbar } from 'notistack';
 
 
 
@@ -30,8 +38,53 @@ export const Login: React.FC = () => {
             padding: '20px 0 30px',
         }});
     const classes=useStyles();
+    const { enqueueSnackbar } = useSnackbar();
+    const [credentials, setCredentials] = useState<Credentials>({
+        email: '',
+        password: '',
+    });
+    const [redirection, setRedirection] = useState(false);
+    let loggedInUser : User | undefined;
+
+    function onChange(e: any) {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value } as Credentials);
+    }
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        const headers = {
+            "Content-Type": "application/json"
+        }
+        axios.post<LoginResponse>('http://localhost:5000/api/auth', credentials, {"headers" : headers})
+            .then((response) => {
+                localStorage.setItem('accessToken', response.data.accessToken);
+                localStorage.setItem('refreshToken', response.data.refreshToken);
+
+                const accessToken = localStorage.getItem('accessToken');
+                const decodedAccessToken: JwtToken = jwt_decode(accessToken);
+                loggedInUser = {
+                    id: +decodedAccessToken.id,
+                    email: decodedAccessToken.email,
+                    firstName: decodedAccessToken.firstName,
+                    lastName: decodedAccessToken.lastName
+                };
+                localStorage.setItem('user',JSON.stringify(loggedInUser));
+                setRedirection(true);
+            })
+            .catch(() => {
+                enqueueSnackbar('Error. Email and/or password incorrect.',{ autoHideDuration: 3000 })
+            })
+
+    }
+    function navigate() {
+        return(
+            <Switch>
+                <Redirect to="/dashboard"/>
+            </Switch>
+        );
+
+    }
     return(
-        <form className={classes.container}>
+        <form className={classes.container} onSubmit={handleSubmit}>
             <h2>Tax Form Generator</h2>
             <div className={classes.registerInput}>
                 <TextField
@@ -42,6 +95,7 @@ export const Login: React.FC = () => {
                     variant="outlined"
                     type="email"
                     required
+                    onChange={onChange}
                 />
                 <TextField
                     className={classes.input}
@@ -51,6 +105,7 @@ export const Login: React.FC = () => {
                     variant="outlined"
                     type="password"
                     required
+                    onChange={onChange}
                 />
                 <div className={classes.links}>
                     <Link to="/register">Create account</Link>
@@ -60,10 +115,12 @@ export const Login: React.FC = () => {
                     variant="contained"
                     size="medium"
                     color="secondary"
+                    type="submit"
                 >
                     Login
                 </Button>
             </div>
+            {redirection && navigate()}
         </form>
     );
 }
